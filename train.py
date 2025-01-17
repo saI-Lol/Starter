32b1d5c1dc5b7f5fe1f7560498c9914a47e91da8
ochwo laban <labanochwo@gmail.com>
2025-01-17 11:04:47 +0300

    added changes

---
+++ train.py
@@ -0,0 +1,182 @@
+# Standard library imports
+import json
+import torch.multiprocessing as mp
+from torch.distributed import destroy_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
+import os
+import warnings
+from collections import Counter
+from functools import partial
+from pathlib import Path
+import tarfile
+import shutil
+import argparse
+from torch.amp import autocast, GradScaler
+import torch.optim.lr_scheduler as lr_scheduler
+# Data manipulation and analysis
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+# Visualization
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+
+# Geospatial libraries
+import geopandas as gpd
+import pyproj
+import pyproj.datadir
+import pystac_client
+import rasterio
+import rioxarray
+import xarray as xr
+from pyproj import CRS, Transformer
+from rasterio.mask import mask
+from rasterio.plot import reshape_as_raster
+from rasterio.transform import from_bounds
+from shapely import wkt
+from shapely.geometry import Polygon, box
+from shapely.wkt import loads
+
+# AWS
+import boto3
+from botocore import UNSIGNED
+from botocore.client import Config
+
+# PyTorch and computer vision
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.transforms import functional as TF
+import torch.optim as optim
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision.models.detection import maskrcnn_resnet50_fpn
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNN, MaskRCNNPredictor
+from torchvision.models.detection.roi_heads import RoIHeads
+from torchvision.transforms import functional as F
+
+# Settings
+pd.set_option("display.max_colwidth", None)
+pd.set_option("display.max_columns", None)
+warnings.filterwarnings("ignore")
+from utils import *
+
+def train_epoch(epoch, num_epochs, model, optimizer, scheduler, train_loader, rank):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    scaler = GradScaler()
+    model.train()
+    iterator = tqdm(train_loader)
+    for batch_idx, batch in enumerate(iterator):
+        imgs, targets = batch
+        imgs = [img.cuda() for img in imgs]
+        targets = [
+            {k: v.cuda() if torch.is_tensor(v) else v for k, v in t.items()}
+            for t in targets
+        ]
+        
+        with autocast(device_type=device):
+            losses_dict = model(imgs, targets)
+            losses = sum(loss for loss in losses_dict.values())
+
+        iterator.set_description(f"Epoch [{epoch+1}/{num_epochs}] Loss: {losses.item():.4f}")
+
+        optimizer.zero_grad()
+        scaler.scale(losses).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+    scheduler.step()
+
+def val_epoch(epoch, num_epochs, model, valid_loader, rank):    
+        model.eval()
+        sum_abs_diff_per_class = {c: 0 for c in range(1, 5)}
+        total_images = 0
+
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(tqdm(valid_loader)):
+                imgs, targets = batch
+                imgs = [img.cuda() for img in imgs]
+                predictions = model(imgs)
+
+                for pred, gt in zip(predictions, targets):
+                    pred_counts = {
+                        c: (pred["labels"] == c).sum().item() for c in range(1, 5)
+                    }
+                    gt_counts = {c: (gt["labels"] == c).sum().item() for c in range(1, 5)}
+
+                    for c in range(1, 5):
+                        diff = abs(pred_counts[c] - gt_counts[c])
+                        sum_abs_diff_per_class[c] += diff
+
+                    total_images += 1
+
+        mae_per_class = {c: sum_abs_diff_per_class[c] / total_images for c in range(1, 5)}
+        overall_mae = sum(mae_per_class.values()) / len(mae_per_class)
+        if rank == 0:
+            print(f"Epoch [{epoch+1}/{num_epochs}] Validation MAE per class: {mae_per_class}")
+            print(
+                f"Epoch [{epoch+1}/{num_epochs}] Validation Overall MAE: {overall_mae:.4f} \n"
+            )
+
+
+def main(rank, world_size, args):
+    ddp_setup(rank, world_size)
+    tier1_df = get_labelled_dataset('tier1')
+    tier3_df = get_labelled_dataset('tier3')
+    train_df = pd.concat([tier1_df, tier3_df])
+    hold_df = get_labelled_dataset('hold')
+    val_df = get_labelled_dataset('test')
+    test_df = get_unlabelled_dataset('predict')
+
+    train_dataset = BuildingDataset(train_df, resize_size=(args.imgsz, args.imgsz))
+    val_dataset = BuildingDataset(val_df, resize_size=(args.imgsz, args.imgsz))
+    holdout_dataset = BuildingDataset(hold_df, resize_size=(args.imgsz, args.imgsz))
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=args.train_batch_size,
+        shuffle=False,
+        num_workers=args.num_workers,
+        collate_fn=collate_fn,
+        sampler = DistributedSampler(train_dataset)
+    )
+
+    valid_loader = DataLoader(
+        val_dataset,
+        batch_size=args.val_batch_size,
+        shuffle=False,
+        num_workers=args.num_workers,
+        collate_fn=collate_fn,
+        sampler = DistributedSampler(val_dataset)
+    )
+
+    model = get_maskrcnn_model(num_classes=5)
+    model = model.cuda()
+    model = model.to(rank)
+    model = DDP(model, device_ids=[rank])
+
+    optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
+    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[15, 29, 43, 53, 65, 80, 90, 100, 110, 130, 150, 170, 180, 190], gamma=0.5)
+    num_epochs = args.epochs
+
+    for epoch in range(num_epochs):
+        train_epoch(epoch, num_epochs, model, optimizer, scheduler, train_loader, rank)
+        val_epoch(epoch, num_epochs, model, valid_loader, rank)
+    destroy_process_group()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Training Starter Notebook model")
+    parser.add_argument("--num-workers", type=int, default=4, help="Number of workers for data loader")
+    parser.add_argument("--train-batch-size", type=int, default=8, help="Batch size for training")
+    parser.add_argument("--val-batch-size", type=int, default=4, help="Batch size for validation")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of epochs to train")
+    parser.add_argument("--imgsz", type=int, default=512, help="Image size for training")
+    args = parser.parse_args()
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size, args), nprocs=world_size)
+
+
---
+++ utils.py
@@ -0,0 +1,254 @@
+# Standard library imports
+import json
+import multiprocessing
+import os
+import warnings
+from collections import Counter
+from functools import partial
+from pathlib import Path
+import tarfile
+import shutil
+
+# Data manipulation and analysis
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+# Visualization
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+
+# Geospatial libraries
+import geopandas as gpd
+import pyproj
+import pyproj.datadir
+import pystac_client
+import rasterio
+import rioxarray
+import xarray as xr
+from pyproj import CRS, Transformer
+from rasterio.mask import mask
+from rasterio.plot import reshape_as_raster
+from rasterio.transform import from_bounds
+from shapely import wkt
+from shapely.geometry import Polygon, box
+from shapely.wkt import loads
+
+# AWS
+import boto3
+from botocore import UNSIGNED
+from botocore.client import Config
+
+# PyTorch and computer vision
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.transforms import functional as TF
+import torch.optim as optim
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision.models.detection import maskrcnn_resnet50_fpn
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNN, MaskRCNNPredictor
+from torchvision.models.detection.roi_heads import RoIHeads
+from torchvision.transforms import functional as F
+from torch.distributed import init_process_group
+
+# Settings
+pd.set_option("display.max_colwidth", None)
+pd.set_option("display.max_columns", None)
+warnings.filterwarnings("ignore")
+
+class Resize(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, image, target):
+        orig_width, orig_height = image.size
+        image = TF.resize(image, self.size)
+
+        ratio_width = self.size[0] / orig_width
+        ratio_height = self.size[1] / orig_height
+
+        if "boxes" in target:
+            boxes = target["boxes"]
+            boxes[:, [0, 2]] = boxes[:, [0, 2]] * ratio_width
+            boxes[:, [1, 3]] = boxes[:, [1, 3]] * ratio_height
+            target["boxes"] = boxes
+
+        if "masks" in target:
+            masks = target["masks"]
+            masks = masks.unsqueeze(1)  # Add channel dimension
+            masks = TF.resize(
+                masks, self.size, interpolation=TF.InterpolationMode.NEAREST
+            )
+            masks = masks.squeeze(1)
+            target["masks"] = masks
+
+        return image, target
+
+
+class BuildingDataset(Dataset):
+    def __init__(self, df, resize_size=(512, 512)):
+        self.df = df
+        self.resize_size = resize_size
+        self.damage_class_to_id = {
+            "no-damage": 1,
+            "minor-damage": 2,
+            "major-damage": 3,
+            "destroyed": 4,
+        }
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        img_path = row.post_image_path
+        label_path = row.post_label_path
+        image_id = torch.tensor([idx])
+
+        with rasterio.open(img_path) as src:
+            img_array = src.read()
+            img_array = np.transpose(img_array, (1, 2, 0)).astype(np.uint8)
+            img = Image.fromarray(img_array)
+
+        width, height = img.size
+
+        with open(label_path, "r") as f:
+            annotations = json.load(f)
+
+        boxes = []
+        labels = []
+        masks = []
+        annotations = annotations["features"]["xy"]
+
+        for annotation in annotations:
+            properties = annotation["properties"]
+            subtype = properties["subtype"]
+            damage_label = self.damage_class_to_id.get(subtype, 0)
+
+            polygon_wkt = annotation["wkt"]
+            polygon = loads(polygon_wkt)
+
+            if not polygon.is_valid:
+                continue
+
+            polygon = self._clip_polygon_to_image(polygon, width, height)
+            if polygon.is_empty:
+                continue
+
+            xmin, ymin, xmax, ymax = polygon.bounds
+            boxes.append([xmin, ymin, xmax, ymax])
+            labels.append(damage_label)
+
+            mask = self._polygon_to_mask(polygon, width, height)
+            masks.append(mask)
+
+        if len(boxes) == 0:
+            target = {
+                "boxes": torch.zeros((0, 4), dtype=torch.float32),
+                "labels": torch.zeros((0,), dtype=torch.int64),
+                "masks": torch.zeros((0, height, width), dtype=torch.uint8),
+                "image_id": image_id,
+            }
+        else:
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+            labels = torch.as_tensor(labels, dtype=torch.int64)
+            masks = torch.as_tensor(np.stack(masks, axis=0), dtype=torch.uint8)
+
+            target = {
+                "boxes": boxes,
+                "labels": labels,
+                "masks": masks,
+                "image_id": image_id,
+            }
+
+        resize_transform = Resize(self.resize_size)
+        img, target = resize_transform(img, target)
+        img = TF.to_tensor(img)
+
+        return {
+            "img": img,
+            "target": target
+        }
+
+    def __len__(self):
+        return len(self.df)
+
+    def _polygon_to_mask(self, polygon, width, height):
+        mask = Image.new("L", (width, height), 0)
+        draw = ImageDraw.Draw(mask)
+
+        if polygon.is_empty:
+            return np.array(mask, dtype=np.uint8)
+
+        if polygon.geom_type == "Polygon":
+            if polygon.exterior is not None:
+                x, y = polygon.exterior.coords.xy
+                coords = [(xi, yi) for xi, yi in zip(x, y)]
+                draw.polygon(coords, outline=1, fill=1)
+        elif polygon.geom_type == "MultiPolygon":
+            for poly in polygon.geoms:
+                if poly.exterior is not None:
+                    x, y = poly.exterior.coords.xy
+                    coords = [(xi, yi) for xi, yi in zip(x, y)]
+                    draw.polygon(coords, outline=1, fill=1)
+
+        return np.array(mask, dtype=np.uint8)
+
+    def _clip_polygon_to_image(self, polygon, width, height):
+        image_box = box(0, 0, width, height)
+        return polygon.intersection(image_box)
+
+
+def get_labelled_dataset(folder_name):
+    data = []
+    root = f"/kaggle/input/{folder_name}-xview-geotiffdata/{folder_name}"
+    for filename in sorted(os.listdir(f"{root}/images")):        
+        if 'post' in filename:
+            ID = '_'.join(filename.split('_')[:-2])
+            image_path = f"{root}/images/{filename}"
+            label_path = f"{root}/labels/{filename.replace('.tif', '.json')}"
+            data.append({
+                'id':ID,
+                'post_image_path':image_path,
+                'post_label_path':label_path
+            })
+    return pd.DataFrame(data)
+
+def get_unlabelled_dataset(folder_name):
+    data = []
+    root = f"/kaggle/input/{folder_name}-xview-geotiffdata"
+    for filename in sorted(os.listdir(f"{root}/Images")):        
+        if 'post' in filename:
+            ID = '_'.join(filename.split('_')[:-2])
+            image_path = f"{root}/Images/{filename}"
+            data.append({
+                'id':ID,
+                'post_image_path':image_path
+            })
+    return pd.DataFrame(data)
+
+def get_maskrcnn_model(num_classes=5):
+    model = maskrcnn_resnet50_fpn(pretrained=True)
+
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(
+        in_features_mask, hidden_layer, num_classes
+    )
+    return model
+
+
+def collate_fn(batch):
+    imgs = [item["img"] for item in batch]
+    targets = [item["target"] for item in batch]
+    return imgs, targets
+
+def ddp_setup(rank: int, world_size: int):
+   os.environ["MASTER_ADDR"] = "localhost"
+   os.environ["MASTER_PORT"] = "12355"
+   torch.cuda.set_device(rank)
+   init_process_group(backend="nccl", rank=rank, world_size=world_size)
