c2762bbd58cc164dc960beed8339a5eefd95b9f2
ochwo laban <labanochwo@gmail.com>
2025-01-21 17:00:24 +0300

    added changes...

---
+++ run.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/saI-Lol/Starter.git
+cd Starter
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python main.py --score-threshold 0.5 --train-batch-size 1 --val-batch-size 1 --epochs 5
